0733eb5bfd37bc8f6d4a899e0a71b49e08e4110a
Sedadalkiran <Sedadalkiran@users.noreply.github.com>
2021-03-02 22:49:38 +0300

    +Game project basics

---
+++ Assets/Scripts/ControlRigidBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class ControlRigidBody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidBodySettings rbSettings;
+
+        private void Update()
+        {
+            bool keyDown = Input.GetKeyDown(KeyCode.Space);
+                if (keyDown)
+               {
+                _rigidbody.AddForce(rbSettings.Force, ForceMode.Impulse);
+                }
+        }
+    }
+}
---
+++ Assets/Scripts/ControlRigidBodySettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter
+{
+    [CreateAssetMenu(menuName ="Scripts/ControlRigidBodySettings")]
+    public class ControlRigidBodySettings: ScriptableObject
+    {
+        [SerializeField] private Vector3 _force;
+
+        public Vector3 Force { get { return _force; } }
+
+    }
+
+}
